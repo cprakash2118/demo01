835a3678ff5216a888c91aea6efe5032cb90f29f
Chandra Prakash <64169976+cprakash2118@users.noreply.github.com>
2022-03-15 16:26:59 +0530

    Update db.sql

--- multicontainer-design.sol/deploy/nodejs/networked/db.sql
+++ multicontainer-design.sol/deploy/nodejs/networked/db.sql
@@ -1,3 +1,4 @@
 CREATE TABLE `Item` (`id` BIGINT not null auto_increment primary key, `description` VARCHAR(100), `done` BIT);
-INSERT INTO `Item` (`id`,`description`,`done`) VALUES (1,'Pick up newspaper', 0);
-INSERT INTO `Item` (`id`,`description`,`done`) VALUES (2,'Buy groceries', 1);
+INSERT INTO `Item` (`id`,`description`,`done`) VALUES (1,'Server Commisisoning', 0);
+INSERT INTO `Item` (`id`,`description`,`done`) VALUES (2,'Server decomm', 1);
+INSERT INTO `Item` (`id`,`description`,`done`) VALUES (3,'QCS DC LABS', 1);
